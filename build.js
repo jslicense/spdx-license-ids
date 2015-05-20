deab5ea0af7636ec7d14993358786e83fe57c042
Kyle Mitchell <kyle@kemitchell.com>
2015-05-20 20:29:42 +0000

    build using the JSON license list on spdx.org
    (#2)

--- build.js
+++ build.js
@@ -2,6 +2,7 @@
 
 const assert = require('assert');
 const fs = require('fs');
+const got = require('got');
 
 const eachAsync = require('each-async');
 const rmrf = require('rm-rf');
@@ -9,26 +10,30 @@ const stringifyObject = require('stringify-object');
 
 const pkg = require('./package.json');
 
-let array = Object.keys(require('spdx-license-list'));
-let files = [
-  {
-    path: pkg.main,
-    contents: JSON.stringify(array, null, '  ')
-  },
-  {
-    path: require('./bower.json').main,
-    contents: `window.spdxLicenseIds = ${stringifyObject(array, {indent: '  '})};`
-  }
-];
+got('http://spdx.org/licenses/licenses.json', {json: true}, (gotError, json) => {
+  assert.ifError(gotError);
 
-rmrf(pkg.name + '*', removeErr => {
-  assert.ifError(removeErr);
+  let array = json.licenses.map(license => license.licenseId);
+  let files = [
+    {
+      path: pkg.main,
+      contents: JSON.stringify(array, null, '  ')
+    },
+    {
+      path: require('./bower.json').main,
+      contents: `window.spdxLicenseIds = ${stringifyObject(array, {indent: '  '})};`
+    }
+  ];
 
-  eachAsync(files, (file, index, next) => {
-    console.log('Writing... ' + file.path);
-    fs.writeFile(file.path, file.contents + '\n', next);
-  }, writeErr => {
-    assert.ifError(writeErr);
-    console.log('Build completed.');
+  rmrf(pkg.name + '*', removeErr => {
+    assert.ifError(removeErr);
+
+    eachAsync(files, (file, index, next) => {
+      console.log('Writing... ' + file.path);
+      fs.writeFile(file.path, file.contents + '\n', next);
+    }, writeErr => {
+      assert.ifError(writeErr);
+      console.log('Build completed.');
+    });
   });
 });
