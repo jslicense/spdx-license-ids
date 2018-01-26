9461692c69569afcf0121ad354a75d796d372a9e
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-01-26 15:21:13 +0900

    update devDependencies
    Highlight: update @shinnn/eslint-config and follow its coding rules, for example tab indent

--- build.js
+++ build.js
@@ -12,9 +12,9 @@ const writeFileAtomically = require('write-file-atomically');
 loudRejection();
 
 (async () => {
-  const filePath = join(__dirname, files[0]);
-  const [ids] = await Promise.all([getSpdxLicenseIds(), rmfr(filePath)]);
-  await writeFileAtomically(filePath, JSON.stringify(ids, null, '  ') + '\n');
+	const filePath = join(__dirname, files[0]);
+	const [ids] = await Promise.all([getSpdxLicenseIds.all(), rmfr(filePath)]);
+	await writeFileAtomically(filePath, `${JSON.stringify(ids, null, '\t')}\n`);
 
-  console.log(green(`Wrote ${filePath}.`));
+	console.log(green(`Wrote ${filePath}.`));
 })();
--- test.js
+++ test.js
@@ -4,6 +4,6 @@ const main = require('.');
 const test = require('tape');
 
 test('require(\'spdx-license-ids\')', t => {
-  t.ok(Array.isArray(main), 'should return an array of SPDX license identifiers.');
-  t.end();
+	t.ok(Array.isArray(main), 'should return an array of SPDX license identifiers.');
+	t.end();
 });
