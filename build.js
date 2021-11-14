1ada865b1c3896a74d338f30872a2db03b5f311a
Kyle E. Mitchell <kyle@kemitchell.com>
2021-11-14 12:44:55 -0800

    Reimplement build.js without get-spdx-license-ids
    get-spdx-license-ids is failing to load request.

--- build.js
+++ build.js
@@ -1,30 +1,38 @@
-'use strict';
+const https = require('https');
+const fs = require('fs');
 
-const {basename, join} = require('path');
-const {writeFile} = require('fs').promises;
+https.get('https://spdx.org/licenses/licenses.json', response => {
+	const chunks = [];
+	response
+	.on('data', chunk => {
+		chunks.push(chunk);
+	})
+	.once('error', error => {
+		throw error;
+	})
+	.once('end', () => {
+		const buffer = Buffer.concat(chunks);
+		const data = JSON.parse(buffer);
+		const {licenses} = data;
+		const index = [];
+		const deprecated = [];
+		for (const {licenseId: id, isDeprecatedLicenseId: isDeprecated} of licenses) {
+			if (id.endsWith('+')) {
+				continue;
+			}
+			if (isDeprecated) {
+				deprecated.push(id);
+			} else {
+				index.push(id);
+			}
+		}
+		index.sort();
+		deprecated.sort();
+		fs.writeFileSync('index.json', stringify(index, null, 2));
+		fs.writeFileSync('deprecated.json', stringify(deprecated, null, 2));
+	});
+});
 
-const {cyan, green} = require('chalk');
-const {files} = require('./package.json');
-const getSpdxLicenseIds = require('get-spdx-license-ids');
-const rmfr = require('rmfr');
-
-module.exports = (async () => {
-	const paths = files.map(filename => join(__dirname, filename));
-
-	const [[valid, deprecated]] = await Promise.all([
-		getSpdxLicenseIds.both(),
-		...paths.map(path => rmfr(path))
-	]);
-
-	valid.sort();
-	deprecated.sort();
-
-	await Promise.all(paths.map(async path => {
-		const data = basename(path) === 'deprecated.json' ? deprecated : valid;
-		await writeFile(path, `${JSON.stringify(data, null, '\t')}\n`);
-
-		console.log(`${green('[OK]')} Created ${cyan(path)}`);
-	}));
-
-	console.log();
-})();
+function stringify(data) {
+	return `${JSON.stringify(data, null, '\t')}\n`;
+}
