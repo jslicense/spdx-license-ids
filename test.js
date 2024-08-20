a278986bc2defd80fdc9c4f174cc1f9beba3ee9b
Kyle E. Mitchell <kyle@kemitchell.com>
2024-08-20 11:52:29 -0700

    Replace Tape with node:test test runner
    This eliminates this package's last npm dependency.

--- test.js
+++ test.js
@@ -1,46 +1,37 @@
-'use strict';
-
+const test = require('node:test');
+const assert = require('assert');
 const deprecated = require('./deprecated');
 const valid = require('.');
-const test = require('tape');
 
-(async () => {
-	await require('./build.js');
+test('require(\'spdx-license-ids\')', t => {
+	assert(Array.isArray(valid), 'should be an array.');
 
-	test('require(\'spdx-license-ids\')', t => {
-		t.ok(Array.isArray(valid), 'should be an array.');
+	assert(
+		valid.includes('LGPL-3.0-or-later'),
+		'should include non-deprecated license IDs.'
+	);
 
-		t.ok(
-			valid.includes('LGPL-3.0-or-later'),
-			'should include non-deprecated license IDs.'
-		);
+	assert(
+		!valid.includes('Nunit'),
+		'should not include deprecated license IDs.'
+	);
+});
 
-		t.notOk(
-			valid.includes('Nunit'),
-			'should not include deprecated license IDs.'
-		);
+test('require(\'spdx-license-ids/deprecated\')', t => {
+	assert(Array.isArray(deprecated), 'should be an array.');
 
-		t.end();
-	});
+	assert(
+		deprecated.length < valid.length,
+		'should be smaller than the main export.'
+	);
 
-	test('require(\'spdx-license-ids/deprecated\')', t => {
-		t.ok(Array.isArray(deprecated), 'should be an array.');
+	assert(
+		deprecated.includes('eCos-2.0'),
+		'should include deprecated license IDs.'
+	);
 
-		t.ok(
-			deprecated.length < valid.length,
-			'should be smaller than the main export.'
-		);
-
-		t.ok(
-			deprecated.includes('eCos-2.0'),
-			'should include deprecated license IDs.'
-		);
-
-		t.notOk(
-			deprecated.includes('ISC'),
-			'should not include non-deprecated license IDs.'
-		);
-
-		t.end();
-	});
-})();
+	assert(
+		!deprecated.includes('ISC'),
+		'should not include non-deprecated license IDs.'
+	);
+});
